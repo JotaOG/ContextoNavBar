2c6ccd82f0df4b31b01daba71594ca2c7248d18f
JotaOG <85458044+JotaOG@users.noreply.github.com>
2021-11-15 03:38:42 -0400

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { MyContext } from "./Context/MyContext";
 import { Wrapper } from "./Components/Wrapper";
 import { NavBar } from "./Components/NavBar";
 import { FormWrapper } from "./Components/FormWrapper";
+import "./App.css";
 
 function App() {
   return (
